6e830980a7c1f47f64e6043cad4f6e0e7f71fd0d
Dmitry Chernous <Hawk1277@yandex.ru>
2025-05-24 19:04:17 +0300

    Добавлены карточки и функция удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,35 @@
 // @todo: Темплейт карточки
+const placesList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+function createCard(card) {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  cardImage.src = card.link;
+  cardImage.alt = card.name;
+  cardTitle.textContent = card.name;
+
+  deleteCard(cardElement);
+
+  return cardElement;
+}
+
+function deleteCard(cardElement) {
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", () => cardElement.remove());
+}
+
+function renderCards() {
+  initialCards.forEach((card) => {
+    const cardElement = createCard(card);
+    placesList.append(cardElement);
+  });
+}
+
+renderCards();
 
 // @todo: DOM узлы
 
